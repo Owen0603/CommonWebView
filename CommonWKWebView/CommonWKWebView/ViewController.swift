42beb2fedc23f10ac1d4d6a43c19deaaf4374d1e
wangsw <845234177@qq.com>
2016-09-05 10:55:19 +0800

    no message

---
+++ CommonWKWebView/CommonWKWebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CommonWKWebView
+//
+//  Created by 姚凤 on 16/9/5.
+//  Copyright © 2016年 姚凤. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
