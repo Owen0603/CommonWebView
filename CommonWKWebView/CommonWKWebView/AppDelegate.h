35c2e7caf1b773bc2e496b3e2c87d721e6f0b09f
wangsw <845234177@qq.com>
2016-09-05 10:51:29 +0800

    no message

---
+++ CommonWKWebView/CommonWKWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommonWKWebView
+//
+//  Created by 姚凤 on 16/9/5.
+//  Copyright © 2016年 姚凤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CommonWKWebView/CommonWKWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CommonWKWebView
+//
+//  Created by 姚凤 on 16/9/5.
+//  Copyright © 2016年 姚凤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
